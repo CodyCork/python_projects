4aefa1807ef961420d77362efd2d6860c21c3b3c
CodyCork <codycork.sfg@outlook.com>
2022-11-02 20:18:11 -0500

    finished

--- functions_practice.py
+++ functions_practice.py
@@ -2,16 +2,29 @@
 def hello():
     print("Hello!")
 
-hello()
+
 #####################################  2  ###############################################
-def pack(item_1, item_2, item_3):
-    pack_list = ["item_1", "item_2", "item_3"]
-    return pack_list
-    print(pack_list)
+def pack(a,b,c):
+  return [a,b,c]
 
 pack("eggs", "bacon", "pancakes")
 #####################################  3  ###############################################
-# def eat_lunch():
+def eat_lunch(my_lst):
+  if len(my_lst) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(my_lst)):
+      if i == 0:
+        print(f"First I eat {my_lst[0]}")
+      else:
+        print(f"Next I eat {my_lst[i]}")
+
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
 
     
 
